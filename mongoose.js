345fc666a1e096d061ce35121f4084e0dd1b8aef
missoumozil@gmail.com <missoumozil@gmail.com>
2019-11-13 01:35:43 +0100

    fixing db  username error

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 //var url = 'mongodb://127.0.0.1:27017/wetext'
-var url = 'mongodb://missoum1307:missoum16@ds059651.mlab.com:59651/wetext'
+var url = 'mongodb://missoum1307:missoum16@ds041248.mlab.com:41248/heroku_656wp5wd'
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
 var db = mongoose.connection;
 db.on('error', (err)=> {
