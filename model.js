c2478d88588c858a5d7351a6eeea8545242129f0
missoum1307 <missoum1307@users.noreply.github.com>
2021-01-13 20:30:54 +0100

    Update model.js

--- model.js
+++ model.js
@@ -1,16 +1,20 @@
-var validator = require('validator')
-var mongoose = require('./mongoose.js')
-
 var userSchema = new mongoose.Schema({
 av: {type: Buffer},
     un: {type: String, required: true, trim: true, unique: true},
+    fn: {type: String, trim: true },
+    ln: {type: String, trim: true },
     em: {type: String, required: true, trim: true, unique: true, validate(value) {
         if (!validator.isEmail(value)) {
             return console.log('not a valid email')
         }
     } },
-    ns: {type: String, required: true, trim: true },
-    pw: {type: String }    
+    pw: {type: String },  
+    ph: {type: Number, trim: true, unique: true, validate(value) {
+        if (!validator.isMobilePhone) {
+            return console.log('not a valid phone number')
+        }
+    } },  
+    ns: {type: String, required: true, trim: true }
   })
 
 var avatarSchema = new mongoose.Schema({
