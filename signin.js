9c18ff8bfc0f4d787a06bd3efc5f89b701f58d10
missoum1307 <missoum1307@users.noreply.github.com>
2021-01-13 21:20:11 +0100

    Update signin.js

--- signin.js
+++ signin.js
@@ -30,7 +30,7 @@ router.post('/signin', /*redirecthome*/ async (req, res) => {
       var match = await bcrypt.compare(password, data.pw)
       if (data.em && match) {
         req.session.username = data.un
-        res.send(`<script>window.parent.postMessage('${data.un}', '*');</script>`)
+        res.send(`<script>window.parent.postMessage('${data.un, req.session.username}', '*');</script>`)
       } else {
         res.send(`<meta http-equiv="refresh" content="1; URL='https://bughunt1307.herokuapp.com/public/signin.html'"/>
         Incorrect password`)
