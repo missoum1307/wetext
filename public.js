a5940002895cd77db449f49adbccb405baa703e1
missoum1307 <missoum1307@users.noreply.github.com>
2021-01-14 17:41:17 +0100

    Create public.js

---
+++ public.js
@@ -0,0 +1,10 @@
+
+var express = require('express')
+var router = express.Router()
+
+
+router.get('/public/logreg', (req, res) => {
+    res.sendFile('/public/logreg.html')
+  })
+
+module.exports = router
