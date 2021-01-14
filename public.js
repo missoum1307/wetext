a647310fe262a3783f3a0ccb4f24df7a6d86c910
missoum1307 <missoum1307@users.noreply.github.com>
2021-01-14 17:47:09 +0100

    Update public.js

--- public.js
+++ public.js
@@ -7,12 +7,12 @@ router.get('/public/logreg', (req, res) => {
     res.sendFile('public/logreg.html', { root : __dirname})
   })
 
-router.get('/public/logreg', (req, res) => {
-    res.sendFile('public/logreg.html', { root : __dirname})
+router.get('/public/reg', (req, res) => {
+    res.sendFile('public/signup.html', { root : __dirname})
   })
 
-router.get('/public/logreg', (req, res) => {
-    res.sendFile('public/logreg.html', { root : __dirname})
+router.get('/public/log', (req, res) => {
+    res.sendFile('public/signin.html', { root : __dirname})
   })
 
 module.exports = router
