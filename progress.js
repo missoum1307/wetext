373609db6e72e4994f48706a3411693ea150eb60
missoum1307 <missoum1307@users.noreply.github.com>
2021-01-15 18:02:09 +0100

    Update progress.js

--- progress.js
+++ progress.js
@@ -4,7 +4,8 @@ var modeluser = require('./model.js')
 var router = express.Router()
 
 router.get('/progress', async (req, res) => {
-  const progressUpdate = await modeluser.findOne({ un: 'missoum1307' })
+  var {username, sid} = req.body
+  const progressUpdate = await modeluser.findOne({ un: username, _id: sid })
   try {
     progressUpdate.pr = [1, 'test']
     await progressUpdate.save()
