3becdf0f06abca5eea41fa21e2650a848475181f
missoum1307 <missoum1307@users.noreply.github.com>
2021-01-16 13:48:19 +0100

    Update progress.js

--- progress.js
+++ progress.js
@@ -4,11 +4,11 @@ var modeluser = require('./model.js')
 var router = express.Router()
 
 router.get('/progress', async (req, res) => {
- // const progressUpdate = await modeluser.findOne({ _id: req.query.sid, un: req.query.username })
+ // const progressUpdate = await modeluser.findOne()
   
 
-const doc = await modeluser.findOne({ _id: '6001d1b20c1a7c00179e6264'});
- console.log(doc);
+const doc = await modeluser.findOne({ _id: req.query.sid, un: req.query.username });
+
 doc.pr.push(5); // Add 5 to the end of the arr
  console.log(doc.pr);
 await doc.save();
@@ -19,7 +19,7 @@ doc.pr.push({
   $each: [1, 3],
   $position: 0
 });
-console.log(doc.pr); // [1, 2, 3, 4, 5]
+console.log(doc.pr[0]); // [1, 2, 3, 4, 5]
   
 /*
   Model.findOne({ name: 'bourne' }, function (err, doc){
