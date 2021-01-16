28172e9a4e66a12079e745a969fe1f12478f0084
missoum1307 <missoum1307@users.noreply.github.com>
2021-01-16 13:00:04 +0100

    Update progress.js

--- progress.js
+++ progress.js
@@ -7,12 +7,10 @@ router.get('/progress', async (req, res) => {
   
   console.log(modeluser)
   
-  const progressUpdate = await modeluser.findOne({ _id: req.query.sid, un: req.query.username })
+  const progressUpdate = await modeluser.findOne({ _id: req.query.sid, un: req.query.username }).update({ _id:  }, { $push: { pr: 'test1' } });
 
-  modeluser.update({ _id: req.query.sid }, { $push: { pr: 'test1' } });
-  modeluser.update({ _id: req.query.sid }, { $push: { wf: 'test5' } });
-  modeluser.update({ _id: req.query.sid }, { $push: { users: 'test6' } });
-  modeluser.update({ _id: req.query.sid }, { $push: { user: 'test7' } });
+  modeluser.user.update({ _id:  }, { $push: { pr: 'test1' } });
+
   try {
     modeluser.update({ _id: req.query.sid }, { $push: { pr: 'test3' } });
     // progressUpdate.pr =  req.query.progress
