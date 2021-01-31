b1b9f8b1ce4f1d731bcd0ece66e79ceed2f9a5d6
missoum1307 <missoum1307@users.noreply.github.com>
2021-01-31 23:28:31 +0100

    Update progress.js

--- progress.js
+++ progress.js
@@ -117,7 +117,7 @@ const players = await modeluser.find({})
 
 let playerObj = []
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < players.length; i++) {
   playerObj.push([players[i]['un'],players[i]['sc']])
 	
 }
