e0c9b3c64a6f4fe3a2e2cea3332df204d6cd3997
missoum1307 <missoum1307@users.noreply.github.com>
2021-02-02 21:51:08 +0100

    Update progress.js

--- progress.js
+++ progress.js
@@ -5,7 +5,11 @@ var bcrypt = require('bcryptjs')
 var router = express.Router()
 router.get('/progress', async (req, res) => {
 const doc = await modeluser.findOne({ _id: req.query.sid, un: req.query.username});
+	
 doc.pr.set(req.query.ps,req.query.progress)
+console.log(req.query.progress)
+console.log(typeof req.query.progress)	
+	
 await doc.save();
  try {
     res.send(200)
